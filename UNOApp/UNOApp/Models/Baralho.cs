ab3fafd294a139d425263706eb773d18777b8304
rubenornelas06 <al2024160108@alunos.utad.pt>
2026-04-03 17:19:29 +0100

    Entrega do Relatório M1 e Modelo de Dados

--- UNOApp/UNOApp/Models/Baralho.cs
+++ UNOApp/UNOApp/Models/Baralho.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using WpfApp1.Models.Enums;
 
 namespace WpfApp1.Models
 {
@@ -7,17 +8,35 @@ namespace WpfApp1.Models
     {
         public List<Carta> Cartas { get; set; } = new List<Carta>();
 
-        public Baralho() { }
+        public Baralho()
+        {
+            InicializarBaralhoBase();
+        }
 
-        // Algoritmo clássico para baralhar as cartas
+        // Inicializar as cartas
+        private void InicializarBaralhoBase()
+        {
+            // Criação básica de algumas cartas só para popular a lista inicialmente
+            foreach (CorCarta cor in Enum.GetValues(typeof(CorCarta)))
+            {
+                if (cor != CorCarta.Nenhuma)
+                {
+                    Cartas.Add(new Carta(Cartas.Count + 1, SimboloCarta.Zero, cor));
+                    Cartas.Add(new Carta(Cartas.Count + 1, SimboloCarta.Um, cor));
+                    Cartas.Add(new Carta(Cartas.Count + 1, SimboloCarta.Compra2, cor));
+                }
+            }
+            // Adiciona coringas
+            Cartas.Add(new Carta(Cartas.Count + 1, SimboloCarta.Coringa, CorCarta.Nenhuma));
+            Cartas.Add(new Carta(Cartas.Count + 1, SimboloCarta.CoringaMais4, CorCarta.Nenhuma));
+        }
+
         public void Baralhar()
         {
             Random rnd = new Random();
             for (int i = 0; i < Cartas.Count; i++)
             {
                 int j = rnd.Next(i, Cartas.Count);
-
-                // Troca as cartas de posição
                 Carta temp = Cartas[i];
                 Cartas[i] = Cartas[j];
                 Cartas[j] = temp;
--- UNOApp/UNOApp/Models/Jogo.cs
+++ UNOApp/UNOApp/Models/Jogo.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections.Generic;
+using WpfApp1.Models.Enums; // Necessário para ver o SentidoJogo
 
 namespace WpfApp1.Models
 {
     public class Jogo
     {
+        // O jogo termina aos 500 pontos
+        public int PontuacaoMaxima { get; set; } = 500;
+
+        // Direção atual do jogo (Horário ou Anti-Horário)
+        public SentidoJogo SentidoAtual { get; set; } = SentidoJogo.Horario;
+
         public List<Jogador> Jogadores { get; set; } = new List<Jogador>();
         public List<int> Pontuacoes { get; set; } = new List<int>();
 
-        // Representa o jogador que tem a vez de jogar
         public Jogador JogadorAtivo { get; set; }
 
         public Mesa Mesa { get; set; } = new Mesa();
